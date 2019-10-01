68e47c6849e033f88e1e81cf0208fe9dcd94d570
Ian <asdasdasd@example.com>
2019-10-01 11:38:53 -0500

    Clase01

---
+++ ejemplo1.py
@@ -0,0 +1,5 @@
+valores = [10,2,11,12,15]
+
+for l in valores:
+	if l%2 == 0:
+		print(l)
---
+++ run.py
@@ -0,0 +1,16 @@
+def metodoUno(n):
+	return n**2
+
+def metodoDos(m):
+	return m+1
+
+def metodoTres(x):
+	return x+100
+
+def metodoCuatro(y):
+	return y*3
+
+print(metodoUno(3))
+print(metodoDos(metodoUno(4)))
+print(metodoTres(metodoDos(metodoTres(5))))
+print(metodoCuatro(metodoTres(metodoDos(metodoUno(2)))))
